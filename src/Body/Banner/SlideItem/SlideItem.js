fe175bb805cce8c66c24f4a3d4f32782a21526f8
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-12-01 12:11:03 -0500

    next thing

--- src/Body/Banner/SlideItem/SlideItem.js
+++ src/Body/Banner/SlideItem/SlideItem.js
@@ -1,12 +1,24 @@
+import { Favorite, PlayArrow } from "@material-ui/icons";
 import React from "react";
 import "./SlideItem.css";
 
-function SlideItem({ name, overview, backdrop }) {
+function SlideItem({ name, overview, backdrop, rating }) {
   return (
     <div className="slideItem">
       <img src={backdrop} alt="" className="slideItem__backdrop" />
       <div className="slideItem__content">
         <h2 className="slideItem__title">{name}</h2>
+        <p>
+          Rating: <strong>{rating}</strong>
+        </p>
+        <div className="slideItem__buttons">
+          <div className="slideItem__btnPlay">
+            <PlayArrow />
+          </div>
+          <div className="slideItem__btnAdd">
+            <Favorite />
+          </div>
+        </div>
         <p className="slideItem__overview">{overview}</p>
       </div>
     </div>
