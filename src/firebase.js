3d088d98ec65cbc904700a80985e58b43c6906c7
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-12-09 19:08:18 -0500

    fixed bugs

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "API_KEY",
+    apiKey: "AIzaSyBzCaPgvBkM_n-17s54V2E8X8WKdH9_R20",
     authDomain: "netflix-publish.firebaseapp.com",
     databaseURL: "https://netflix-publish.firebaseio.com",
     projectId: "netflix-publish",
     storageBucket: "netflix-publish.appspot.com",
     messagingSenderId: "450410592911",
-    appId: "APP_ID",
+    appId: "1:450410592911:web:945447fc131a4eb1f0cefa",
     measurementId: "G-R2E0FYQHH3"
 })
 
